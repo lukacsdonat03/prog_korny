bbf17832d143d4e3582f905e209244d9318bd395
Lukács Donát <lukacsdonatxxx@gmail.com>
2025-05-01 20:30:40 +0200

    Initial commit

---
+++ src/main/java/hu/nye/prog_korny/ProgKornyApplication.java
@@ -0,0 +1,13 @@
+package hu.nye.prog_korny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgKornyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgKornyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/nye/prog_korny/ProgKornyApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.prog_korny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgKornyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
